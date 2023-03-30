b9cea2a141a9fab1e58fee30465831178b20948f
moong2 <www.castlehi@gmail.com>
2023-03-30 18:41:54 +0900

    feat: add user entity

---
+++ src/main/java/com/Saojung/whatisthis/domain/User.java
@@ -0,0 +1,28 @@
+package com.Saojung.whatisthis.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String password;
+    private String name;
+    private int age;
+    private String parent_password;
+
+    public User(String id, String password, String name, int age, String parent_password) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.parent_password = parent_password;
+    }
+}
