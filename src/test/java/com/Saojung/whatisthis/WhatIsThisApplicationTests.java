6e4cece021f3441843e5343cff5615720c16b5e9
moong2 <www.castlehi@gmail.com>
2023-03-30 18:00:33 +0900

    initial commit

---
+++ src/test/java/com/Saojung/whatisthis/WhatIsThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Saojung.whatisthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
