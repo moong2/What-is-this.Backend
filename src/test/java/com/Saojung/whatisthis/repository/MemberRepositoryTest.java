d6de6e5b380473beffefdd662546c0cfb6bea7ba
moong2 <www.castlehi@gmail.com>
2023-04-06 14:58:42 +0900

    test: add join test

--- src/test/java/com/Saojung/whatisthis/repository/MemberRepositoryTest.java
+++ src/test/java/com/Saojung/whatisthis/repository/MemberRepositoryTest.java
@@ -1,4 +1,66 @@
+package com.Saojung.whatisthis.repository;
+
+import com.Saojung.whatisthis.domain.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DuplicateKeyException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 class MemberRepositoryTest {
-  
+
+    @Autowired private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입")
+    public void 회원가입() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertEquals(member.getId(), save_member.getId());
+    }
+
+    @Test
+    @DisplayName("Null Attribute 테스트")
+    public void Null_확인() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .parent_password("p_password")
+                .build();
+
+        //when
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertEquals(member.getId(), save_member.getId());
+    }
+
+    @Test
+    @DisplayName("NotNull Attribute 테스트")
+    public void NotNull_확인() {
+        assertThrows(NullPointerException.class, () -> {
+            Member member = Member.builder()
+                    .id("castlehi")
+                    .password("password")
+                    .name("박성하")
+                    .age(24)
+                    .build();
+        });
+    }
 }
