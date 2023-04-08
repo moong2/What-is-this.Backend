bbf331406653ea92a33f87ab4255d5699354acc2
moong2 <www.castlehi@gmail.com>
2023-04-08 12:58:39 +0900

    test: add tests about login

--- src/test/java/com/Saojung/whatisthis/repository/MemberRepositoryTest.java
+++ src/test/java/com/Saojung/whatisthis/repository/MemberRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DuplicateKeyException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -82,5 +81,165 @@ class MemberRepositoryTest {
 
         //then
         assertEquals(find_member.getId(), save_member.getId());
+    }
+
+    @Test
+    @DisplayName("<id, pw> 동일 체크 성공")
+    public void 로그인_성공() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        String id = "castlehi";
+        String pw = "password";
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertEquals(save_member.getId(), id);
+        assertEquals(save_member.getPassword(), pw);
+    }
+
+    @Test
+    @DisplayName("<id, pw> 중 id 동일 체크 실패")
+    public void 로그인_실패_id() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        String id = "wrong";
+        String pw = "password";
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertNotEquals(save_member.getId(), id);
+        assertEquals(save_member.getPassword(), pw);
+    }
+
+    @Test
+    @DisplayName("<id, pw> 중 pw 동일 체크 실패")
+    public void 로그인_실패_pw() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        String id = "castlehi";
+        String pw = "wrong";
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertEquals(save_member.getId(), id);
+        assertNotEquals(save_member.getPassword(), pw);
+    }
+
+    @Test
+    @DisplayName("부모 pw 동일 체크 성공")
+    public void 부모페이지_로그인_성공_pw() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        String p_pw = "p_password";
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertEquals(save_member.getParent_password(), p_pw);
+    }
+
+    @Test
+    @DisplayName("부모 pw 동일 체크 실패")
+    public void 부모페이지_로그인_실패_pw() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        String p_pw = "wrong";
+        Member save_member = memberRepository.save(member);
+
+        //then
+        assertNotEquals(save_member.getParent_password(), p_pw);
+    }
+
+    @Test
+    @DisplayName("회원 정보 조회")
+    public void 회원_정보_조회() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        Member save_member = memberRepository.save(member);
+        Member find_member = memberRepository.getReferenceById(String.valueOf(save_member.getIdx()));
+
+        //then
+        assertEquals(find_member.getId(), member.getId());
+        assertEquals(find_member.getPassword(), member.getPassword());
+        assertEquals(find_member.getName(), member.getName());
+        assertEquals(find_member.getAge(), member.getAge());
+        assertEquals(find_member.getParent_password(), member.getParent_password());
+    }
+
+    @Test
+    @DisplayName("회원 정보 변경")
+    public void 회원_정보_변경() {
+        //given
+        Member member = Member.builder()
+                .id("castlehi")
+                .password("password")
+                .name("박성하")
+                .age(24)
+                .parent_password("p_password")
+                .build();
+
+        //when
+        Member save_member = memberRepository.save(member);
+
+        save_member = Member.builder()
+                .id(member.getId())
+                .password("change_password")
+                .name(member.getName())
+                .age(save_member.getAge() + 1)
+                .parent_password(member.getParent_password())
+                .build();
+        Member change_member = memberRepository.save(save_member);
+
+        //then
+        assertEquals(change_member.getId(), member.getId());
+        assertNotEquals(change_member.getPassword(), member.getPassword());
+        assertEquals(change_member.getAge(), member.getAge() + 1);
     }
 }
