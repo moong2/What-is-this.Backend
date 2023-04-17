048f9b0edae02019eb1f81134527cd13e185b19e
moong2 <www.castlehi@gmail.com>
2023-04-17 14:56:39 +0900

    fix: change injection method

--- src/main/java/com/Saojung/whatisthis/service/MemberService.java
+++ src/main/java/com/Saojung/whatisthis/service/MemberService.java
@@ -1,14 +1,12 @@
-//package com.Saojung.whatisthis.service;
-//
-//import com.Saojung.whatisthis.repository.MemberRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class MemberService {
-//
-//    private final MemberRepository memberRepository;
-//
-//
-//}
+package com.Saojung.whatisthis.service;
+
+import com.Saojung.whatisthis.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
