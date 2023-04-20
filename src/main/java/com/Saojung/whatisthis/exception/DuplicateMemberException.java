373fcc726d04f7e44bbb78c9c1de629be690e7bb
moong2 <www.castlehi@gmail.com>
2023-04-20 19:25:19 +0900

    feat: add exception

--- src/main/java/com/Saojung/whatisthis/exception/DuplicateMemberException.java
+++ src/main/java/com/Saojung/whatisthis/exception/DuplicateMemberException.java
@@ -1,2 +1,22 @@
-package com.Saojung.whatisthis.exception;public class DuplicateMemberException {
+package com.Saojung.whatisthis.exception;
+
+public class DuplicateMemberException extends RuntimeException{
+    public DuplicateMemberException() {
+    }
+
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+
+    public DuplicateMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateMemberException(Throwable cause) {
+        super(cause);
+    }
+
+    public DuplicateMemberException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
