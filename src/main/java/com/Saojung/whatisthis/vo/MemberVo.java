c812f22e6da3c82fece5cd5ee8a42e1922c3fd0d
moong2 <www.castlehi@gmail.com>
2023-04-24 19:45:13 +0900

    feat: add MemberVo

---
+++ src/main/java/com/Saojung/whatisthis/vo/MemberVo.java
@@ -0,0 +1,37 @@
+package com.Saojung.whatisthis.vo;
+
+import com.Saojung.whatisthis.domain.Amends;
+import com.Saojung.whatisthis.domain.Analysis;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class MemberVo {
+    @NotNull
+    private Long idx;
+    @NotNull
+    private String id;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birth;
+    @NotNull
+    private String parentPassword;
+
+    public MemberVo(Long idx, @NonNull String id, @NonNull String password, @NonNull String name, LocalDate birth, @NonNull String parentPassword) {
+        this.idx = idx;
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.birth = birth;
+        this.parentPassword = parentPassword;
+    }
+}
