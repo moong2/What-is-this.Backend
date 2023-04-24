59e2fd80a14ae142016edb03b754a7b2c9d4d546
moong2 <www.castlehi@gmail.com>
2023-04-24 19:45:28 +0900

    feat: add MemberController

--- src/main/java/com/Saojung/whatisthis/controller/MemberController.java
+++ src/main/java/com/Saojung/whatisthis/controller/MemberController.java
@@ -1,2 +1,44 @@
-package com.Saojung.whatisthis.controller;public class MemberController {
+package com.Saojung.whatisthis.controller;
+
+import com.Saojung.whatisthis.dto.MemberDto;
+import com.Saojung.whatisthis.service.MemberService;
+import com.Saojung.whatisthis.vo.LoginVo;
+import com.Saojung.whatisthis.vo.MemberVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public String signUp(@RequestBody MemberDto memberDto) {
+        try {
+            MemberDto resultDto = memberService.signUp(memberDto);
+            return resultDto.getName() + "님의 회원가입이 완료되었습니다.";
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginVo loginVo) {
+        try {
+            MemberDto member = memberService.login(loginVo);
+
+            MemberVo memberVo = new MemberVo(member.getIdx(), member.getId(), member.getPassword(), member.getName(), member.getBirth(), member.getParentPassword());
+
+            return new ResponseEntity<>(memberVo, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
