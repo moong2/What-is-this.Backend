a89875d1e5f6d7c622879de9c701da254fed8888
moong2 <www.castlehi@gmail.com>
2023-04-24 18:24:41 +0900

    style: change LoginDto to LoginVo

--- src/main/java/com/Saojung/whatisthis/vo/LoginVo.java
+++ src/main/java/com/Saojung/whatisthis/vo/LoginVo.java
@@ -1,4 +1,4 @@
-package com.Saojung.whatisthis.dto;
+package com.Saojung.whatisthis.vo;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -6,13 +6,13 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-public class LoginDto {
+public class LoginVo {
     @NotNull
     private String id;
     private String password;
     private String parentPassword;
 
-    public LoginDto(@NonNull String id, String password, String parentPassword) {
+    public LoginVo(@NonNull String id, String password, String parentPassword) {
         this.id = id;
         this.password = password;
         this.parentPassword = parentPassword;
