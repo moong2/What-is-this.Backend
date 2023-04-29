a799f448b99b03d66efbaae9c7553c7cadb38bb0
moong2 <www.castlehi@gmail.com>
2023-04-29 15:01:49 +0900

    feat: add exceptions to use in WordService

--- src/main/java/com/Saojung/whatisthis/exception/LevelException.java
+++ src/main/java/com/Saojung/whatisthis/exception/LevelException.java
@@ -1,2 +1,23 @@
-package com.Saojung.whatisthis.exception;public class LevelException {
+package com.Saojung.whatisthis.exception;
+
+public class LevelException extends RuntimeException{
+    public LevelException() {
+        super();
+    }
+
+    public LevelException(String message) {
+        super(message);
+    }
+
+    public LevelException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LevelException(Throwable cause) {
+        super(cause);
+    }
+
+    protected LevelException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/com/Saojung/whatisthis/exception/NoWordException.java
+++ src/main/java/com/Saojung/whatisthis/exception/NoWordException.java
@@ -1,2 +1,22 @@
-package com.Saojung.whatisthis.exception;public class NoWordException {
+package com.Saojung.whatisthis.exception;
+
+public class NoWordException extends RuntimeException{
+    public NoWordException() {
+    }
+
+    public NoWordException(String message) {
+        super(message);
+    }
+
+    public NoWordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoWordException(Throwable cause) {
+        super(cause);
+    }
+
+    public NoWordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
