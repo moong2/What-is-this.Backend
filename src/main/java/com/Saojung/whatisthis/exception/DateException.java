f3ea9b479614cece7ff373aa1b36ec53129b4540
moong2 <www.castlehi@gmail.com>
2023-04-29 15:16:59 +0900

    feat: add exception to use in WordService

--- src/main/java/com/Saojung/whatisthis/exception/DateException.java
+++ src/main/java/com/Saojung/whatisthis/exception/DateException.java
@@ -1,2 +1,23 @@
-package com.Saojung.whatisthis.exception;public class DateException {
+package com.Saojung.whatisthis.exception;
+
+public class DateException extends RuntimeException{
+    public DateException() {
+        super();
+    }
+
+    public DateException(String message) {
+        super(message);
+    }
+
+    public DateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DateException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DateException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
