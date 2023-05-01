d50a426b967eda35924367a13f065dcd45c573d1
moong2 <www.castlehi@gmail.com>
2023-05-01 17:51:04 +0900

    test: add getAllWord controller test

--- src/test/java/com/Saojung/whatisthis/controller/WordControllerTest.java
+++ src/test/java/com/Saojung/whatisthis/controller/WordControllerTest.java
@@ -1,4 +1,96 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.Saojung.whatisthis.controller;
+
+import com.Saojung.whatisthis.domain.Member;
+import com.Saojung.whatisthis.dto.MemberDto;
+import com.Saojung.whatisthis.dto.WordDto;
+import com.Saojung.whatisthis.service.MemberService;
+import com.Saojung.whatisthis.service.WordService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class WordControllerTest {
-  
+
+    MockMvc mvc;
+
+    @Autowired
+    private static WebApplicationContext wac;
+    @Autowired
+    static ObjectMapper objectMapper;
+
+    @Autowired
+    WordService wordService;
+    @Autowired
+    MemberService memberService;
+
+    @BeforeEach
+    void beforeEach() {
+        JacksonTester.initFields(this, objectMapper);
+        this.mvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+
+        List<MemberDto> members = memberService.getMembers();
+        for (MemberDto member : members) {
+            memberService.withdraw(member.getIdx());
+        }
+    }
+
+    @Test
+    @DisplayName("모든 단어 전달 테스트")
+    void 모든_단어_전달() throws Exception {
+        //given
+        Member member = Member.builder()
+                .idx(1L)
+                .userId("castlehi")
+                .password("password")
+                .name("박성하")
+                .birth(LocalDate.of(2000, 06, 17))
+                .parentPassword("p_password")
+                .build();
+
+        MemberDto memberDto = MemberDto.from(member);
+
+        WordDto wordDto = new WordDto(
+                null, "사과", 2, 1, LocalDateTime.of(2023, 04, 28, 13, 52 ,00), member
+        );
+
+        memberService.signUp(memberDto);
+
+        //when
+        //then
+        mvc.perform(get("/" + memberDto.getIdx() + "/words")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.idx", notNullValue()))
+                .andExpect(jsonPath("$.member_idx", notNullValue()))
+                .andDo(print());
+    }
 }
