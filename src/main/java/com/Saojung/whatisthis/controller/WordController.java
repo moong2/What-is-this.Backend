7bd8f4396db523897bd62f50f45e6fef4ca83c1c
moong2 <www.castlehi@gmail.com>
2023-05-02 17:42:01 +0900

    feat: add util and method to use WordController

--- src/main/java/com/Saojung/whatisthis/controller/WordController.java
+++ src/main/java/com/Saojung/whatisthis/controller/WordController.java
@@ -1,7 +1,71 @@
 package com.Saojung.whatisthis.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Saojung.whatisthis.domain.Member;
+import com.Saojung.whatisthis.dto.MemberDto;
+import com.Saojung.whatisthis.dto.WordDto;
+import com.Saojung.whatisthis.service.MemberService;
+import com.Saojung.whatisthis.service.WordService;
+import com.Saojung.whatisthis.vo.WordVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class WordController {
+
+    private final WordService wordService;
+    private final MemberService memberService;
+
+    @PostMapping("/studyWord")
+    public ResponseEntity studyWord(@RequestParam Long member_idx, @RequestBody WordVo wordVo) {
+        try {
+            MemberDto returnDto = memberService.findMember(member_idx);
+            Member member = Member.builder()
+                    .idx(returnDto.getIdx())
+                    .userId(returnDto.getUserId())
+                    .password(returnDto.getPassword())
+                    .name(returnDto.getName())
+                    .birth(returnDto.getBirth())
+                    .parentPassword(returnDto.getParentPassword())
+                    .amends(returnDto.getAmends())
+                    .analysis(returnDto.getAnalysis())
+                    .build();
+
+            WordDto wordDto = WordDto.builder()
+                    .word(wordVo.getWord())
+                    .level(wordVo.getLevel())
+                    .successLevel(wordVo.getSuccessLevel())
+                    .date(wordVo.getDate())
+                    .member(member)
+                    .build();
+
+            wordService.create(wordDto);
+
+            return new ResponseEntity<>("단어 학습이 완료되었습니다.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/getWords")
+    public ResponseEntity getWords(@RequestParam Long member_idx) {
+        try {
+            List<WordDto> words = wordService.getWordsByDate(member_idx);
+
+            List<WordVo> wordVos = new ArrayList<>();
+            for (WordDto word : words) {
+                WordVo wordVo = new WordVo(word.getIdx(), word.getWord(), word.getLevel(), word.getSuccessLevel(), word.getDate());
+                wordVos.add(wordVo);
+            }
+
+            return new ResponseEntity<>(wordVos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/Saojung/whatisthis/vo/WordVo.java
+++ src/main/java/com/Saojung/whatisthis/vo/WordVo.java
@@ -1,2 +1,37 @@
-package com.Saojung.whatisthis.vo;public class WordVo {
+package com.Saojung.whatisthis.vo;
+
+import com.Saojung.whatisthis.domain.Member;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class WordVo {
+    private Long idx;
+    @NotNull
+    private String word;
+    @NotNull
+    private Integer level;
+    @NotNull
+    private Integer successLevel;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date;
+
+    public WordVo(Long idx, @NonNull String word, @NonNull Integer level, @NonNull Integer successLevel, @NonNull LocalDateTime date) {
+        this.idx = idx;
+        this.word = word;
+        this.level = level;
+        this.successLevel = successLevel;
+        this.date = date;
+    }
 }
